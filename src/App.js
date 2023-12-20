3bc3ccf4dfc88cf2aa4f43f54a7b40db2f94488a
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-12-20 21:39:21 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: '',
+      author: ''
+    };
+
+    this.loadQuote = this.loadQuote.bind(this);
+  }
+  loadQuote() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.content + " | " + data.author);
+    
+        this.setState({
+          quote: data.content,
+          author: data.author
+        })
+      })
+  }
+  render() {
+    return (
+      <div id='quote-box' onLoad={this.loadQuote}>
+        <h2 id='text'>"{this.state.quote}</h2>
+        <p id='author'>-{this.state.author}</p>
+        <button id='new-quote' onClick={this.loadQuote}>New quote</button>
+        <a id='tweet-quote' href="#index" target='_blank'></a>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function loadQuote() {
+//   fetch("https://api.quotable.io/random")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       App.setState({
+//         quote: data.content,
+//         author: data.author
+//       })
+//     })
+// }
