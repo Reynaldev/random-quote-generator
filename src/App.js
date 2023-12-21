62d1d7f1cb7f0acadbf8dcad7f4524b2be4985e0
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-12-21 20:10:19 +0700

    Finished

--- src/App.js
+++ src/App.js
@@ -13,13 +13,15 @@ class App extends React.Component {
     this.loadQuote = this.loadQuote.bind(this);
     this.loadQuote();
   }
+  // Load quotes from https://api.quotable.io/random and parse it as JSON. 
+  // Then, set the state from the parsed JSON. 
   loadQuote() {
     fetch("https://api.quotable.io/random")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        console.log(data.content + " | " + data.author);
+        // console.log(data.content + " | " + data.author);
     
         this.setState({
           quote: data.content,
@@ -29,16 +31,21 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div id='quote-box'>
-        <h2 id='text'>"{this.state.quote}</h2>
+      <div id='quote-box' class="container">
+        <h3 id='text'>"{this.state.quote}</h3>
         
         <p id='author'>-{this.state.author}</p>
 
-        <button id='new-quote' onClick={this.loadQuote}>New quote</button>
-
-        <a id='tweet-quote' href="https://twitter.com/intent/tweet" target='_blank'>
-          <i class="fa-brands fa-x-twitter"></i>
-        </a>
+        <div class="row">
+          <div class="col">
+            <button id='new-quote' class="btn btn-dark" onClick={this.loadQuote}>New quote</button>
+          </div>
+          <div class="col-sm-2">
+            <a id='tweet-quote' class="btn btn-dark" href="https://twitter.com/intent/tweet" target='_blank'>
+              <i class="fa-brands fa-x-twitter"></i>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
